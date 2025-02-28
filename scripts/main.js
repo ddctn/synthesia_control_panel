2ad0daf62e8630ebdca73991f1ebcb993ccfd954
ddctn <avociafuture@gmail.com>
2025-02-28 03:06:16 +0300

    ass files

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let btn1 = document.querySelector(".choosebtn");
+let btn2 = document.querySelector(".createbtn");
+let rtrn = document.querySelector(".rtrn");
+
+let sections = document.querySelectorAll(".mainsec, .choose, .create");
+
+function action(s) {
+  sections.forEach(function (sec) {
+    if (sec.classList.contains(s)) {
+      sec.classList.add("block");
+      sec.classList.remove("none");
+    } else {
+      sec.classList.remove("block");
+      sec.classList.add("none");
+    }
+  });
+}
+
+btn1.addEventListener("click", function () {
+  action("choose");
+});
+
+btn2.addEventListener("click", function () {
+  action("create");
+});
+
+rtrn.addEventListener("click", function () {
+  action("mainsec");
+});
